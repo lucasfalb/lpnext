79eb5ff0bb98ec28337644452dc9caeb172844df
lucasfalb <lucasdefrancaalbuquerque@gmail.com>
2024-02-10 13:30:12 +0000

    new components update

--- src/components/FAQ/index.tsx
+++ src/components/FAQ/index.tsx
@@ -16,7 +16,7 @@ export default function FAQ({ title, questions }: FAQProps) {
       <h2 className="text-4xl h-fit font-bold text-darkBlueCp max-w-[762px] sticky top-4">{title}</h2>
       <div className="flex flex-col gap-6">
         {questions && questions.map((question, index) => (
-          <Accordion title={question.question}>
+          <Accordion key={index} title={question.question}>
             {question.answer}
           </Accordion>
         ))}
